af1a3c63b9fc20b930668c555c8e088bcf5d3ac4
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-10-12 23:47:33 -0400

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'cream': '#d5bdaf',
+      'light-green': '#a3b18a',
+      'green': '#588157',
+      'light-purple': '#cdb4db',
+      'blue': '#a2d2ff',
+      'light-blue': '#bde0fe',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
