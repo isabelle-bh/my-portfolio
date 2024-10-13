b2df240c362174dccff1e9f2135676884cfb2c76
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-10-12 23:48:21 -0400

    putting everything together

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
+import Home from './components/home';
 import About from './components/about';
 import Projects from './components/projects';
 import Contact from './components/contact';
 import Footer from './components/footer';
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <About />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App bg-light-green flex flex-col min-h-screen varela-round-regular">
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
