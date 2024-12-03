e3346e819110ab4095572c43562a234266b2bc09
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-12-03 02:44:51 -0500

    feat: new resume section

---
+++ src/components/resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import me from "../assets/me.jpeg";
+
+const Resume = () => {
+  return (
+    <section className="resume">
+      <h2 className="tagline">Check out my resume and experience.</h2>
+      <div className="resume-content">
+
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
