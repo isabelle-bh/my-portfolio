1420c215f9fb675ac965d658819d922022af6ae5
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-12-03 02:45:46 -0500

    chore: adding routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header';
 import Home from './components/home';
 import About from './components/about';
 import Projects from './components/projects';
+import Resume from './components/resume';
 import Contact from './components/contact';
 import Footer from './components/footer';
 import "./App.css";
@@ -12,13 +13,14 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <div className="App bg-light-green flex flex-col min-h-screen varela-round-regular">
+      <div className="App flex flex-col min-h-screen varela-round-regular">
         <Header />
         <main>
           <Routes>
-            <Route path='/my-portfolio' element={<Home />} />
+            <Route path='/my-portfolio/home' element={<Home />} />
             <Route path='/my-portfolio/about' element={<About />} />
             <Route path='/my-portfolio/projects' element={<Projects />} />
+            <Route path='/my-portfolio/resume' element={<Resume />} />
             <Route path='/my-portfolio/contact' element={<Contact />} />
           </Routes>
         </main>
