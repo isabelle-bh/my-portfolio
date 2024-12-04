55273b558e569097d8a5d929163e2994849a41f0
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-12-04 18:53:58 -0500

    chore: updating description

--- src/components/about.js
+++ src/components/about.js
@@ -11,8 +11,7 @@ const About = () => {
         </div>
         <div className="text-container">
           <ul className="about-list">
-            <li><span>Hello, my name is Isabelle!</span></li>
-            <li><span>I am currently pursuing my software engineering degree at Western University.</span></li>
+            <li><span>My name is Isabelle. I am currently pursuing my software engineering degree at Western University!</span></li>
             <li><span>I have touched on many different fields of SWE, including full stack web development, reinforcement learning in autonomous vehicles, game development, and more...</span></li>
             <li><span>Some of my hobbies include Formula 1, drawing and reading, yoga, video games, and spending time with friends / family!</span></li>
           </ul>
