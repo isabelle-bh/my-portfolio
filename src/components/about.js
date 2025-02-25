d6b4a3c330ac0c5c7b9681782358e4b4b9944e98
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-02-25 01:04:39 -0500

    some style changes

--- src/components/about.js
+++ src/components/about.js
@@ -4,16 +4,16 @@ import me from "../assets/isa1.jpg";
 const About = () => {
   return (
     <section className="about">
-      <h2 className="tagline">Learn more About Me!</h2>
+      <h2 className="tagline">learn more about me!</h2>
       <div className="about-content">
         <div className="image-container">
-          <img src={me} alt="Isabelle" />
+          <img src={me} alt="Isabelle" loading="eager" />
         </div>
         <div className="text-container">
           <ul className="about-list">
-            <li><span>My name is Isabelle. I am currently pursuing my software engineering degree at Western University!</span></li>
-            <li><span>I have touched on many different fields of SWE, including full stack web development, reinforcement learning in autonomous vehicles, game development, and more...</span></li>
-            <li><span>Some of my hobbies include Formula 1, drawing and reading, yoga, video games, and spending time with friends / family!</span></li>
+            <li><span>My name is Isabelle. I am currently pursuing my software engineering degree at Western University.</span></li>
+            <li><span>I have worked on projects in many different areas of software engineering, including full stack web development, machine learning and autonomous systems, game development, and more.</span></li>
+            <li><span>Some of my personal hobbies include Formula 1, drawing and reading, skiing, yoga, video games, and spending time with friends / family!</span></li>
           </ul>
         </div>
       </div>
