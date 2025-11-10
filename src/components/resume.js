4713c467821eeb44d12a32ecd197a2d0f8b19c75
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-11-10 15:30:28 +0100

    docs: changing resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import myResume from "../assets/IsabelleBeaudry_Resume2025.pdf"; 
+import myResume from "../assets/Beaudry_Isabelle_Resume.pdf"; 
 
 const Resume = () => {
   const [isError, setIsError] = useState(false);
@@ -14,7 +14,7 @@ const Resume = () => {
       <h2 className="tagline">check out my resume and experience!</h2>
       <p>
         For my contact information, please send me an email through this form {' '}
-        <a href="https://isabelle-bh.github.io/my-portfolio/contact">here</a>.
+        <strong><a href="https://isabelle-bh.github.io/my-portfolio/contact">here</a></strong>.
       </p>
       <div className="resume-content">
         {isError ? (
